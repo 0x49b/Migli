0fa98a39ee2e26acaaa13f2772c2acea476f1a8b
florian <florianjeremias@gmail.com>
2020-09-19 11:52:01 +0200

    won

--- Migli/LoadingDots.swift
+++ Migli/LoadingDots.swift
@@ -10,22 +10,22 @@ import SwiftUI
 
 struct LoadingDots: View {
     
-    @State private var shouldAnimate = true // false
+    @State private var shouldAnimate = false
     
     var body: some View {
         HStack {
             Circle()
-                .fill(Color.blue)
+                .fill(ColorManager.migrosOrange)
                 .frame(width: 20, height: 20)
                 .scaleEffect(shouldAnimate ? 1.0 : 0.5)
                 .animation(Animation.easeInOut(duration: 0.5).repeatForever())
             Circle()
-                .fill(Color.blue)
+                .fill(ColorManager.migrosOrange)
                 .frame(width: 20, height: 20)
                 .scaleEffect(shouldAnimate ? 1.0 : 0.5)
                 .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.3))
             Circle()
-                .fill(Color.blue)
+                .fill(ColorManager.migrosOrange)
                 .frame(width: 20, height: 20)
                 .scaleEffect(shouldAnimate ? 1.0 : 0.5)
                 .animation(Animation.easeInOut(duration: 0.5).repeatForever().delay(0.6))
